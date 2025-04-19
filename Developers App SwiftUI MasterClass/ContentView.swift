c596bb8ae8b9c8d697133b1edb96b93ef89427b9
Aran Fononi <aaronfononi@gmail.com>
2025-04-19 13:12:59 +0330

    Initial Commit

---
+++ Developers App SwiftUI MasterClass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Developers App SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 19/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Developers App SwiftUI MasterClass/Developers_App_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Developers_App_SwiftUI_MasterClassApp.swift
+//  Developers App SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 19/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Developers_App_SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
