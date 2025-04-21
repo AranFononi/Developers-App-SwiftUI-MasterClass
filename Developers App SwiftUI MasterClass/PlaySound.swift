e90752e6c89949d63cf684f098155af2941125fe
Aran Fononi <aaronfononi@gmail.com>
2025-04-21 13:16:52 +0330

    Create AVFoundation Function to play sounds

---
+++ Developers App SwiftUI MasterClass/PlaySound.swift
@@ -0,0 +1,23 @@
+//
+//  PlaySound.swift
+//  Developers App SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 21/4/25.
+//
+
+import SwiftUI
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
