edcee66268274fbdabd82a66d9ef6577e1b1168b
Aran Fononi <aaronfononi@gmail.com>
2025-04-21 13:23:53 +0330

    Add Animations to cards

--- Developers App SwiftUI MasterClass/CardView.swift
+++ Developers App SwiftUI MasterClass/CardView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct CardView: View {
     var card : Card
+    @State private var fadeIn: Bool = false
+    @State private var move: Bool = false
     
     var body: some View {
         ZStack {
             Image(card.imageName)
+                .opacity(fadeIn ? 1 : 0)
             
             VStack {
                 Text(card.title)
@@ -26,7 +29,7 @@ struct CardView: View {
                     .multilineTextAlignment(.center)
                     .italic()
             }
-            .offset(y: -218)
+            .offset(y: move ? -218 : -300)
             
             Button {
                 playSound(sound: "sound-chime", type: "mp3")
@@ -48,12 +51,20 @@ struct CardView: View {
                 .clipShape(Capsule())
                 .shadow(color: .colorShadow, radius: 6)
             }
-            .offset(y: 210)
+            .offset(y: move ? 210 : 300)
         } //: ZStack
         .frame(width: 335, height: 545)
         .background(LinearGradient(colors: card.gradientColors, startPoint: .top, endPoint: .bottom))
         .clipShape(.rect(cornerRadius: 16))
         .shadow(radius: 8)
+        .onAppear {
+            withAnimation(.linear(duration: 1.2)) {
+                fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.5)) {
+                move.toggle()
+            }
+        }
     }
 }
 
